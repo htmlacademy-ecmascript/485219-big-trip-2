60f3549234d25c455b9d2e557462576e4ef26cba
DESKTOP-QH0U1BD\Kharlamov <ilya.kharlamov.92@mail.ru>
2025-02-10 03:42:51 +0300

    Добавляет модули filter-form-view, filter-future-view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import {render} from './render.js';
+import {render, RenderPosition} from './render.js';
 import RenderFilterEverythingView from './view/filter-everything-view.js';
+import RenderFilterFutureView from './view/filter-future-view.js';
+import RenderFilterFormView from './view/filter-form-view.js';
 
-const pageHeaderElement = document.querySelector('.page-header');
-const filterControlsContainerElement = pageHeaderElement.querySelector('.trip-controls__filters');
+const tripControlsFilterElement = document.querySelector('.trip-controls__filters');
+const filterFormElement = new RenderFilterFormView();
 
-render(new RenderFilterEverythingView(), filterControlsContainerElement);
-
-
+render(filterFormElement, tripControlsFilterElement, RenderPosition.AFTERBEGIN);
+render(new RenderFilterEverythingView(), filterFormElement.getElement());
+render(new RenderFilterFutureView(), filterFormElement.getElement());
