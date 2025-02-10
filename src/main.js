4d13167e1bd3c73530dce1ac1023e55a2b80c5da
DESKTOP-QH0U1BD\Kharlamov <ilya.kharlamov.92@mail.ru>
2025-02-10 04:34:48 +0300

    переименовывает классы

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import {render, RenderPosition} from './render.js';
-import RenderFilterEverythingView from './view/filter-everything-view.js';
-import RenderFilterFutureView from './view/filter-future-view.js';
-import RenderFilterFormView from './view/filter-form-view.js';
+import FilterEverythingView from './view/filter-everything-view.js';
+import FilterFutureView from './view/filter-future-view.js';
+import FilterFormView from './view/filter-form-view.js';
 import FilterPresentView from './view/filter-present-view.js';
 import FilterPastView from './view/filter-past-view.js';
 
 const tripControlsFilterElement = document.querySelector('.trip-controls__filters');
-const filterFormElement = new RenderFilterFormView();
+const filterFormElement = new FilterFormView();
 
 render(filterFormElement, tripControlsFilterElement, RenderPosition.AFTERBEGIN);
-render(new RenderFilterEverythingView(), filterFormElement.getElement());
-render(new RenderFilterFutureView(), filterFormElement.getElement());
+render(new FilterEverythingView(), filterFormElement.getElement());
+render(new FilterFutureView(), filterFormElement.getElement());
 render(new FilterPresentView(), filterFormElement.getElement());
 render(new FilterPastView(), filterFormElement.getElement());
