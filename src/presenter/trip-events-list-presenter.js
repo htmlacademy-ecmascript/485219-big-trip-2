eaf35c4ddeef9454a3206fdb8cf54f7362e13bcd
DESKTOP-QH0U1BD\Kharlamov <ilya.kharlamov.92@mail.ru>
2025-02-16 00:13:21 +0300

    синхронизация между ПК

--- src/presenter/trip-events-list-presenter.js
+++ src/presenter/trip-events-list-presenter.js
@@ -1,11 +1,14 @@
 import {render, RenderPosition} from '../render.js';
 import TripEventsListView from '../view/trip-events-list-view.js';
+import EventsItemView from '../view/trip-events-item-view.js';
 
 const tripEventsSectionElement = document.querySelector('.trip-events');
 const tripEventsListElement = new TripEventsListView();
+// const tripEventsItemElement = new EventsItemView();
 
 export default class TripEventsList {
   init() {
     render(tripEventsListElement, tripEventsSectionElement, RenderPosition.BEFOREEND);
+    render(new EventsItemView(), tripEventsListElement.getElement());
   }
 }
