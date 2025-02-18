e32d24eabc993fe65dc4887345f04d0fb56b398b
DESKTOP-QH0U1BD\Kharlamov <ilya.kharlamov.92@mail.ru>
2025-02-18 22:35:14 +0300

    Добавляет форму редактирования точки маршрута

--- src/presenter/trip-events-list-presenter.js
+++ src/presenter/trip-events-list-presenter.js
@@ -1,5 +1,6 @@
 import {render, RenderPosition} from '../render.js';
 import TripEventsListView from '../view/trip-events-list-view.js';
+import EventsItemEditView from '../view/trip-events-item-edit-view.js';
 import EventsItemView from '../view/trip-events-item-view.js';
 
 const tripEventsSectionElement = document.querySelector('.trip-events');
@@ -9,6 +10,7 @@ const renderCount = 3;
 export default class TripEventsList {
   init() {
     render(tripEventsListElement, tripEventsSectionElement, RenderPosition.BEFOREEND);
+    render(new EventsItemEditView(), tripEventsListElement.getElement());
     for (let i = 0; i < renderCount; i++) {
       render(new EventsItemView(), tripEventsListElement.getElement());
     }
