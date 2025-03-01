6dc96b8fb579eb6bd69285a661841afb1ab456df
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2025-03-01 06:51:50 +0300

    синхронизация между пк

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import TripFilterPresenter from './presenter/trip-filter-presenter.js';
 import TripTimeFilterPresenter from './presenter/trip-time-filter-presenter.js';
 import TripEventsListPresenter from './presenter/trip-events-list-presenter.js';
 
+const tripEventsSectionElement = document.querySelector('.trip-events');
 const eventsModel = new EventModel();
 const TripFilter = new TripFilterPresenter();
 const TripEventsTimeFilter = new TripTimeFilterPresenter();
-const TripEventsList = new TripEventsListPresenter(eventsModel);
+const TripEventsList = new TripEventsListPresenter(eventsModel, tripEventsSectionElement);
 
 TripFilter.init();
 TripEventsTimeFilter.init();
--- src/presenter/trip-events-list-presenter.js
+++ src/presenter/trip-events-list-presenter.js
@@ -3,33 +3,57 @@ import TripEventsListView from '../view/trip-events-list-view.js';
 import EventsItemEditView from '../view/trip-events-item-edit-view.js';
 import EventsItemView from '../view/trip-events-item-view.js';
 
-const tripEventsSectionElement = document.querySelector('.trip-events');
+// const tripEventsSectionElement = document.querySelector('.trip-events');
 const tripEventsListElement = new TripEventsListView();
 const renderCount = 3;
 
 export default class TripEventsList {
+  #eventsModel;
+  #listContainer;
+  #eventsListPoints;
 
-  constructor(eventsModel) {
-    this.eventsModel = eventsModel;
+  // constructor(listContainer) {
+  //   this.#listContainer = tripEventsListElement;
+  // }
+
+  constructor(eventsModel, listContainer) {
+    this.#eventsModel = eventsModel;
+    this.#listContainer = listContainer;
   }
 
   init() {
-    this.eventsListPoints = [...this.eventsModel.getPoints()];
+    this.#eventsListPoints = [...this.#eventsModel.getPoints()];
+    this.#renderEventsListPoints();
 
-    render(tripEventsListElement, tripEventsSectionElement, RenderPosition.BEFOREEND);
+    // render(tripEventsListElement, this.#listContainer, RenderPosition.BEFOREEND);
 
-    render(new EventsItemEditView({
-      point: this.eventsListPoints[0],
-      selectedOffers: [...this.eventsModel.getSelectedOffers(this.eventsListPoints[0].type, this.eventsListPoints[0].offers)],
-      availableOffers: [...this.eventsModel.getOffersByType(this.eventsListPoints[0].type)]
-    }), tripEventsListElement.element);
+    // render(new EventsItemEditView({
+    //   point: this.eventsListPoints[0],
+    //   selectedOffers: [...this.eventsModel.getSelectedOffers(this.eventsListPoints[0].type, this.eventsListPoints[0].offers)],
+    //   availableOffers: [...this.eventsModel.getOffersByType(this.eventsListPoints[0].type)]
+    // }), tripEventsListElement.element);
 
-    for (let i = 1; i < renderCount; i++) {
-      render(new EventsItemView({
-        point: this.eventsListPoints[i],
-        offers: [...this.eventsModel.getSelectedOffers(this.eventsListPoints[i].type, this.eventsListPoints[i].offers)],
-        destination: this.eventsModel.getDestinationById(this.eventsListPoints[i].destination),
-      }), tripEventsListElement.element);
-    }
+    // for (let i = 0; i < renderCount; i++) {
+    //   render(new EventsItemView({
+    //     point: this.eventsListPoints[i],
+    //     offers: [...this.#eventsModel.getSelectedOffers(this.eventsListPoints[i].type, this.eventsListPoints[i].offers)],
+    //     destination: this.#eventsModel.getDestinationById(this.eventsListPoints[i].destination),
+    //   }), tripEventsListElement.element);
+    // }
   }
+
+  #renderEventsListPoints() {
+    render(tripEventsListElement, this.#listContainer, RenderPosition.BEFOREEND);
+    this.eventsListPoints.forEach((tripEvent) => {
+      this.#renderTripEvent();
+    })
+  }
+
+  #renderTripEvent(tripEvent) {
+    const offers = "";
+    const destination = "";
+    const offerType = "";
+    const destinationsAll = "";
+  }
+
 }
