78e9d0b31c4e84e8445a2e1cad17dcfdd1a563f1
Ilya Kharlamov <ilya.kharlamov.92@mail.ru>
2025-03-05 12:30:12 +0300

    добавляет перенос строки в темплейте

--- src/view/trip-events-empty-view.js
+++ src/view/trip-events-empty-view.js
@@ -1,7 +1,8 @@
 import AbstractView from '../framework/view/abstract-view';
 
 function createEventsEmptyViewTemplate() {
-  return `<p class="trip-events__msg">Click New Event to create your first point</p>`;
+  return `<p class="trip-events__msg">
+    Click New Event to create your first point</p>`;
 }
 
 export default class EventsEmptyView extends AbstractView {
